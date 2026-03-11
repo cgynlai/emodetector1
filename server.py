4e00c83dfcd7ead2d5c0d2d026c6795092ae69fe
cgynlai <cgynlai@gmail.com>
2026-03-11 05:25:03 -0400

    task 6

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app : TODO
+app = Flask("Emotion Detector")
+
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    """
+    Retrieve the text to analyze from the request arguments
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    items = []
+    for key, value in response.items():
+        items.append(f"'{key}': {value}")
+    
+    joined_text = "For the given statement, the system response is " + ",".join(items)
+
+    return joined_text
+
+    """
+
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+
+    # Return a formatted string with the sentiment label and score
+    return (
+            "The given text has been identified as {} "
+            "with a score of {}.".format(label.split('_')[1], score)
+    """
+    
+
+@app.route("/")
+def render_index_page():
+    """
+    rendering index page
+    """
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
